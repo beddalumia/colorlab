2330bb2d6355ca23438ccad5452184ec4240d4b4
Gabriele Bellomia <gbellomia@live.it>
2022-05-05 05:21:24 +0200

    Intermediate: automation of crameri_update through dir()
    • Inspired by postDMFT.get_list(), slighty touched on the flight
    
    • We cannotˆ currently exclude from the list the +BLABLA folders, so we
      manage the exception with a try-catch construct… flexible and reliable
      > but we might be able to exploit the fact that MATLAB indeed treats
        folders starting with '+' as special (i.e. namespaces), and genpath
        function apparently excludes them apriori, so… let's give it a try!
    
    ————————————————————————————————————————————————————————————————————————
    ˆOnly god knows why:
    
     - {true} gives {[1]}
    
     - strfind(subfolders,'+') gives {[1]}, {[1]}, {empty}, ...
    
     > why ismember() cannot deal with it?!

--- +plotDMFT/+colorlab/+crameri/private/crameri_update.m
+++ +plotDMFT/+colorlab/+crameri/private/crameri_update.m
@@ -2,20 +2,32 @@
 
 % • Unzip the latest release from http://doi.org/10.5281/zenodo.1243862 (†)
 % • Navigate to the resulting dìrectory: …/ScientificColourMaps*/ (* = version)
-% • Update the file list f{:} below, as needed. [TODO: automate this task!]
 % • Run the following:
 
  clear()
 
+ subentries = dir('.'); % Retrieves every subdir and file in current directory 
+ subfolders = subentries([subentries(:).isdir]); % Keeps only subdirectories
+ subfolders = {subfolders(:).name}; % Convert to appropriate cell array
+ subfolders = subfolders(~ismember(subfolders,{'.','..'})); % . ..
+ % subfolders = subfolders(~ismember(strfind(subfolders,'+'),{true})); [BROKEN]
+
  f = {'acton','bam','bamO','bamako','batlow','batlowK','batlowW','berlin','bilbao','broc','brocO',...
       'buda','bukavu','cork','corkO','davos','devon','fes','grayC','hawaii','imola','lajolla','lapaz',...
       'lisbon','nuuk','oleron','oslo','roma','romaO','tofino','tokyo','turku','vanimo','vik','vikO'}; 
- 
- for k = 1:length(f)
-    load([f{k},'/',f{k},'.mat']);
+
+ for k = 1:length(subfolders)
+      if ~ismember(f,subfolders{k})
+         fprintf(1,"'%s' is a candidate new colormap!\n",subfolders{k})
+      end
+      try
+         load([subfolders{k},'/',subfolders{k},'.mat']);
+      catch
+         fprintf(2,"'%s' is not a colormap: omitted from import\n",subfolders{k})
+      end
  end
  
- clear('f','k') 
+ clear('subentries','subfolders','f','k') 
 
  save('CrameriColourMaps.mat') % Move the .mat archive to the original path [Automate?]
 
