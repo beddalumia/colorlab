59c23ac1187e0c2b91beb1cb2fed37d07e499ca7
Gabriele Bellomia <gbellomia@live.it>
2022-08-18 08:40:29 +0200

    Add Mapping Toolbox test to CI
    I've just realized that Mapping Toolbox is provided in the github runner!
    
    > test for worldmap painting via contourcmap/preset_palette interface.

---
+++ .test/worldmaps.m
@@ -0,0 +1,51 @@
+disp('TESTING interaction with Mapping Toolbox [preset_palette]')
+
+colorlab.enter
+close all
+clear
+
+% build the worldmap 
+topographer()
+% preselect the colorscheme.
+preset_palette(@palette.viridis); 
+% apply the preselected colormap
+colorizer('viridis')
+% preselect the colorscheme.
+preset_palette(@palette.cubehelix,3,-0.5,1.5,1	); 
+% apply the preselected colormap
+colorizer('cubehelix')
+% preselect the colorscheme.
+preset_palette(@palette.brewer,'-PuOr'); 
+% apply the preselected colormap
+colorizer('-PuOr')
+% preselect the colorscheme.
+preset_palette(@palette.cmocean,'topo'); 
+% apply the preselected colormap
+colorizer('topo')
+% preselect the colorscheme.
+preset_palette(@palette.crameri,'oleron'); 
+% apply the preselected colormap
+colorizer('oleron')
+
+
+
+disp('>> All good!')
+disp('------------')
+
+function topographer()
+    load topo
+    load coastlines
+    disp('Worldmap building...')
+    worldmap(topo, topolegend);
+    contourfm(topo, topolegend);
+    disp('Worldmap inking...')
+    plotm(coastlat, coastlon, 'k'); 
+    disp('Worldmap painting...')
+end
+
+function colorizer(name)
+    disp(name)
+    contourcmap('preset_palette', 'Colorbar','on', 'Location','horizontal',...
+    'TitleString','Contour Intervals in Meters'); pause(0.5)
+    disp('...done!')
+end
