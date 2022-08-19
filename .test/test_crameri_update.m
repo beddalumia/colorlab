65dcea881bc316de37e39dc3f69e500645ce1b0b
Gabriele Bellomia <gbellomia@live.it>
2022-08-20 01:07:01 +0200

    Fix: `test_crameri_update`
    We need to retrieve the latest version after the previous, or all subsequent
    tests would fail just because crameri dataset is not updated. Dumb me.

--- .test/test_crameri_update.m
+++ .test/test_crameri_update.m
@@ -8,10 +8,10 @@ function upgrade_crameri()
     root = erase(fileparts(mfilename('fullpath')),'.test');
     cd(root)
     cd('crameri')
-    disp('Release 7 [zenodo::5501399]')
-    crameri_update(7,5501399)
     disp('Release 6 [zenodo::4153113]')
     crameri_update('6','4153113')
+    disp('Release 7 [zenodo::5501399]')
+    crameri_update(7,5501399)
     cd(here)
 
     disp('>> All good!')
